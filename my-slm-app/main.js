e923cdcb752929430bf71e659b393c292e7a5075
J.Bae <hyeonlim42@gmail.com>
2026-07-12 13:43:47 +0900

    Initialize Electron app with main window setup

--- my-slm-app/main.js
+++ my-slm-app/main.js
@@ -1,1 +1,25 @@
+const { app, BrowserWindow } = require('electron');
 
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 900,
+        height: 700,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
